b43816b93756102010ce0edb9c9b0d493de8b82f
elijahsamuels <elijahsamuels@gmail.com>
2020-11-25 16:43:26 -0700

    commit tree 7b9af6bb622c73c04a711f664f5f6790b1cf5265

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    extend FancyDance::ClassMethods #extend is used to add functionality to our classes via class methods. We can now call the metadata class method on the Dancer and Kid classes
+    include FancyDance::InstanceMethods #include is used to add functionality to our classes via instance methods
+
+    extend MetaDancing
+    include Dance
+
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+
+       
+end 
